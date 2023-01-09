9ab2e200065b92a932a0df6ccd45078c0ae42897
lhpmso <122245323+lhpmso@users.noreply.github.com>
2023-01-09 12:43:36 -0500

    Update URL.js

--- LHP_Hospital_DW/ClientApp/src/actions/URL.js
+++ LHP_Hospital_DW/ClientApp/src/actions/URL.js
@@ -8,6 +8,8 @@ export function url(partial) {
             return 'https://lhp-ws.azurewebsites.net/api/' + partial
         case 'lhp-hospital-dw-development.azurewebsites.net':
             return 'https://lhp-ws-development.azurewebsites.net/api/' + partial
+        case 'https://lhp-hospital-dw-aetna-prod.azurewebsites.net':
+            return 'https://lhp-ws-aetna-prod.azurewebsites.net/api/' + partial 
         default:
             return 'https://localhost:5002/api/' + partial
     }
